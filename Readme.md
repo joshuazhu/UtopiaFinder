5f5a826afe194c1e6f112bf972767c201416c646
joshuazhu <joshuazhu1989@gmail.com>
2021-02-02 21:43:32 +1100

    basic setup

---
+++ Readme.md
@@ -0,0 +1,5 @@
+### Run offline
+`sls offline start`
+
+### Deploy to Prod
+`NODE_ENV=production sls deploy --stage prod`
--- lambda.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-const apolloServer = require('./dist/aws-wrapper')
-exports.handler = apolloServer.createHandler({
-    cors: {
-        origin: true,
-        credentials: true,
-    },
-});
